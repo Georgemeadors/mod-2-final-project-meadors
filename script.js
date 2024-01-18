2f5502576919070238e1fc7b1662149d486a136f
George <gmeadors@alphaworks.tech>
2024-01-18 15:12:04 -0800

    html and js changes to the selectors

--- script.js
+++ script.js
@@ -4,34 +4,45 @@ const quoteSelector = document.getElementById("quote-selector")
 const displayQuoteButton = document.getElementById("btn")
 const displayQuoteContainer = document.getElementById("quote-container")
 const arrayQuotes = [
-        
+    
+    //this is motivationl
+
     {Text: "Believe that you can and you're halfway there", author: "Theodore Roosevelt"},
     {Text: "Without a sense of urgency,desire loses its value", author: "Jim Rohn"},
     {Text: "Success is not final, failure is not fatal", author: "Winston Churchill"},
     {Text: "Have faith, have faith, when you have nothing else have faith", author: "Francine Rivers"},
+   
+   
+   //love and relationships
+   
     {Text: "To love is nothing. To be loved is something. But to love and be loved is everything", author: "T. Tolis"},
     {Text: "I have decided to stick to love; hate is too great a burden to bear", author: "Martin Luther King Jr"},
     {Text: "The walls we build around us to keep out the sadness, also keeps out the joy", author: "Jim Rohn"},
     {Text: "All of me loves all of you", author: "John Legend"},
+   
+   
+   //wisdom and life
+   
     {Text: "Always do your best. What you plant now, you will harvest later", author: "Og Mandino"},
     {Text: "You will face many defeats in life, but never let yourself be defeated", author: "Maya Angelou"},
     {Text: "The future belongs to those who prepare for it today", author: "Malcolm X"},
     {Text: "The great gift of the human imagination is that it has no limits or ending", author: "Jim Rohn."},
+    
+    
+    //Humorous
+    
     {Text: "The great gift of the human imagination is that it has no limits or ending", author: "W.C Fields"},
     {Text: "I drink to make other people more interesting", author: "Ernest Hemingway"},
     {Text: "As you get older, three things happen. The first is your memory goes, and I can't remember the other two", author: "Sir Norman Wisdom"},
-    {Text: "he best way to appreciate your job is to imagine yourself without  one", author: "Oscar Wilde"},
+    {Text: "The best way to appreciate your job is to imagine yourself without  one", author: "Oscar Wilde"},
+    
+ 
+];
 
- ]
-   //function to update the html dynamically in the future if needed 
-   function updateHTML() {
-    //add additional code here if needed
-   }
-//call the function for the html if needed
 
  function displaySelectedQuote() {
     
-    const selectedIndex=quoteSelector.value
+    const selectedIndex=quoteSelector.value;
 
     const selectedQuote = arrayQuotes[selectedIndex];
    
@@ -49,6 +60,7 @@ const arrayQuotes = [
 displayQuoteButton.addEventListener("click", displaySelectedQuote);
 
 displaySelectedQuote();
+
 
 
 
